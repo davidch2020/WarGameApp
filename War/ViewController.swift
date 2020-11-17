c86a6ab85f0ac29bbf5fdfc332ffa02441fd475e
David <davidncho11@gmail.com>
2020-11-17 09:59:47 -0500

    Initial Commit

---
+++ War/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by David Cho on 11/17/20.
+//  Copyright © 2020 David Cho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
